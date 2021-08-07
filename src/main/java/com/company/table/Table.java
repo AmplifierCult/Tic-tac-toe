e6dd4a868eef0c1be2b380b7282447ffe1854b1b
Andrey Vasilev <tribotechnique@mail.ru>
2021-08-07 10:34:10 +0300

    Realize Table class.

--- src/main/java/com/company/table/Table.java
+++ src/main/java/com/company/table/Table.java
@@ -1,4 +1,31 @@
 package com.company.table;
 
+import java.util.Arrays;
+
 public class Table {
+    private final Object [][] table;
+    private int count;
+
+    public Table() {
+        table = new Object[3][3];
+    }
+
+    public Object getCell(int index1, int index2) {
+        return table[index1][index2];
+    }
+
+    public Object setCell(int index1, int index2, Boolean value) {
+        Object cell = table[index1][index2];
+        if (cell == null) {
+            table[index1][index2] = value;
+            count++;
+            return value;
+        } else {
+            return null;
+        }
+    }
+
+    public int getNumberOfRecords() {
+        return count;
+    }
 }
